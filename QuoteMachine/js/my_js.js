351c91c1370d5e744b684973104f06dd3dcbdca4
Denys Lins <denyslins@gmail.com>
2016-02-23 01:02:00 -0300

    Include QuoteMachine project

---
+++ QuoteMachine/js/my_js.js
@@ -0,0 +1,22 @@
+/**
+ * Created by DenysLins on 22/02/16.
+ */
+
+function getQuote() {
+    var output = $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+        type: 'POST',
+        data: {},
+        dataType: 'json',
+        success: function (data) {
+            document.getElementById("text").innerHTML = data.quote;
+            document.getElementById("author").innerHTML = data.author;
+        },
+        error: function (err) {
+            alert(err);
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "Qlgnm0T4FWmshfjo2kZow7HeVg0Op1VTYLvjsnwD2JCsataLMe"); // Enter here your Mashape key
+        }
+    });
+}
